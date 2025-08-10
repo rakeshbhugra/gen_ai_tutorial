84429da921b52f858a2c03620a34ebd4ff2dfbeb
RakeshBhugra-Vurbalize <rakesh@vurbalize.com>
2025-08-10 14:00:12 +0530

    checkpoint

--- 05-sales-support-agents/helpers/litellm_call.py
+++ 05-sales-support-agents/helpers/litellm_call.py
@@ -1,11 +1,10 @@
 from litellm import completion
-import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
 messages = [{ "content": "Hello, how are you?","role": "user"}]
 
-response = completion(model="gemini/gemini-1.5-flash", messages=messages)
+response = completion(model="gemini/gemini-2.5-flash-lite", messages=messages)
 
 print(response["choices"][0]["message"]["content"])
