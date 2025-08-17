4e87d76c473a0bcdeefd5093206770715cc063b4
RakeshBhugra-Vurbalize <rakesh@vurbalize.com>
2025-08-17 12:10:23 +0530

    created create_embeddings_of_chunk function

--- 06-RAG/main.py
+++ 06-RAG/main.py
@@ -1,12 +1,18 @@
 from helper import read_document, chunk_document
+from embeddings_helper import create_embeddings
 
 
 # Worklow:
-# Read the document
-# Chunking the document text
+# Read the document - done
+# Chunking the document text - done
 # Creating embeddings for the chunk
 # Adding the emebddings to the chromdb collection
 
+
+def create_embeddings_of_chunk(chunk):
+    embeddings = create_embeddings(chunk)
+    return embeddings['embedding']
+    
 
 if __name__ == "__main__":
     document_path = "data_files/Capabl Customer Support Guide.docx"
@@ -16,5 +22,7 @@ if __name__ == "__main__":
     chunks = chunk_document(document)
 
     for chunk in chunks:
-        print(chunk)
+        # print(chunk)
+        embeddings = create_embeddings_of_chunk(chunk)
+        print(f"Chunk: {chunk[:50]}...\nEmbedding: {embeddings[:10]}...")
         print("-"*40)
