091ad56fd5de92a671aaec18604aa66276180880
RakeshBhugra-Vurbalize <rakesh@vurbalize.com>
2025-08-17 13:49:53 +0530

    basic llm helper created

---
+++ 06-RAG/llm_helper.py
@@ -0,0 +1,24 @@
+from litellm import completion
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def get_llm_response(messages):
+    response = completion(
+        messages=messages,
+        model="gemini/gemini-1.5-flash"
+    )
+
+    return response['choices'][0]['message']['content']
+
+if __name__ == "__main__":
+    messages = []
+
+    system_prompt = """You are a helpful assistant, answer under 30 words."""
+    messages.append({"role": "system", "content": system_prompt})
+
+    user_query = "What is the process for handling customer complaints?"
+    messages.append({"role": "user", "content": user_query})
+
+    response = get_llm_response(messages)
+    print("LLM Response:", response)
