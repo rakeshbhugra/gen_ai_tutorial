1191bfc90f079607ff2d421fd2a42c7d0841b041
RakeshBhugra-Vurbalize <rakesh@vurbalize.com>
2025-08-17 14:08:50 +0530

    traditional RAG complete

--- 06-RAG/main.py
+++ 06-RAG/main.py
@@ -1,6 +1,7 @@
 from helper import add_document_to_chromadb
 from embeddings_helper import create_embeddings
 from chromadb_helper import setup_chromadb
+from llm_helper import get_llm_response_from_messages
 
 # take query input
 # create query embedding
@@ -32,6 +33,23 @@ if __name__ == "__main__":
     similar_chunks = find_similar_chunks(query)
 
     print("Similar chunks found:")
+    # for chunk in similar_chunks:
+    #     print(f" - {chunk}...")
+    #     print("-" * 40)
+
+    messages = []
+        
+    # Augment the system prompt
+    system_prompt = "You are support assistant for the company Capabl. Answer the user's query based on the search results from the company support guide."
+    # Add similar chunks to messages
+    system_prompt += "\n\nHere are some relevant sections from the support guide:"
     for chunk in similar_chunks:
-        print(f" - {chunk}...")
-        print("-" * 40)
+        system_prompt += f"\n\n{chunk}"
+
+    messages.append({"role": "system", "content": system_prompt})
+
+    # Add user query to messages
+    messages.append({"role": "user", "content": query})
+
+    response = get_llm_response_from_messages(messages)
+    print("LLM Response:", response)
