eacd707fb9f9cd37ea9b6d9494cf1d6e3a047006
RakeshBhugra-Vurbalize <rakesh@vurbalize.com>
2025-08-17 13:28:07 +0530

    simplified main bloc

--- 06-RAG/main.py
+++ 06-RAG/main.py
@@ -8,30 +8,28 @@ from chromadb_helper import setup_chromadb
 # Augment the system prompt
 # Get response from the LLM
 
-def find_similar_chunks(embeddings):
+def find_similar_chunks(query):
+    # create query embedding
+    embeddings = create_embeddings(query)['embedding']
+
+    # Find similar chunks using vector search in our chromadb collection
     _, collection = setup_chromadb()
     results = collection.query(
         query_embeddings=[embeddings],
         n_results=2
     )
+
     return [x for x in results['documents'][0]]
     
 
 if __name__ == "__main__":
     document_path = "data_files/Capabl Customer Support Guide.docx"
     document_name = "Support Guide"
-
     # add_document_to_chromadb(document_path, document_name)
 
     # query = input("Enter your query: ")
     query = "What is the process for handling customer complaints?"
-
-    # create query embedding
-    embeddings = create_embeddings(query)['embedding']
-
-    # Find similar chunks using vector search in our chromadb collection
-    similar_chunks = find_similar_chunks(embeddings)
-    
+    similar_chunks = find_similar_chunks(query)
 
     print("Similar chunks found:")
     for chunk in similar_chunks:
