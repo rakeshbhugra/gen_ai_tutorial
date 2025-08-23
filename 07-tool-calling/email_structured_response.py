da9048cfb0c5bc0c1ba0e51b2cb910d13edea993
RakeshBhugra-Vurbalize <rakesh@vurbalize.com>
2025-08-23 12:18:56 +0530

    appending user input to conversation history

--- 07-tool-calling/email_structured_response.py
+++ 07-tool-calling/email_structured_response.py
@@ -8,37 +8,37 @@ load_dotenv()
 ####### Manual Parsing Approach ########
 ########################################
 
-conversation_history = [
-    {"role": "system", "content": "You are an expert email writer. My name is John Doe. My manager's"},
-]
+# conversation_history = [
+#     {"role": "system", "content": "You are an expert email writer. My name is John Doe. My manager's"},
+# ]
 
-user_input = """
-Write an email to my manager about being late to work because of traffic.
-email of my manager is: manager@example.com
-name of my manager is: Jane Smith
+# user_input = """
+# Write an email to my manager about being late to work because of traffic.
+# email of my manager is: manager@example.com
+# name of my manager is: Jane Smith
 
-give me output in this format:
-To:
-Subject:
-Body:
-"""
+# give me output in this format:
+# To:
+# Subject:
+# Body:
+# """
 
-conversation_history.append({"role": "user", "content": user_input})
+# conversation_history.append({"role": "user", "content": user_input})
 
-response = completion(
-    model="openai/gpt-5-mini",
-    messages=conversation_history,
-)
+# response = completion(
+#     model="openai/gpt-5-mini",
+#     messages=conversation_history,
+# )
 
-raw_response = response['choices'][0]['message']['content']
-# print("Raw AI Response:", raw_response)
+# raw_response = response['choices'][0]['message']['content']
+# # print("Raw AI Response:", raw_response)
 
-# parse the response into the Email model
-to_email = raw_response.split("To:")[1].split("Subject:")[0].strip()
-subject = raw_response.split("Subject:")[1].split("Body:")[0].strip()
-body = raw_response.split("Body:")[1].strip()
+# # parse the response into the Email model
+# to_email = raw_response.split("To:")[1].split("Subject:")[0].strip()
+# subject = raw_response.split("Subject:")[1].split("Body:")[0].strip()
+# body = raw_response.split("Body:")[1].strip()
 
-print(f"To: {to_email}\nSubject: {subject}\nBody: {body}s")
+# print(f"To: {to_email}\nSubject: {subject}\nBody: {body}s")
 
 # How we will use it
 # send_email(
@@ -74,8 +74,10 @@ email of my manager is: manager@example.com
 name of my manager is: Jane Smith
 """
 
+conversation_history.append({"role": "user", "content": user_input})
+
 response = completion(
-    model="openai/gpt-5-mini",
+    model="openai/gpt-4.1-mini",
     messages=conversation_history,
     response_format=Email  # Specify the Pydantic model for structured response
 )
