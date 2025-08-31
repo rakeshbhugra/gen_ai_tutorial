c2c4041ee8367c8988d5e051e18f27a9eaefb94b
RakeshBhugra-Vurbalize <rakesh@vurbalize.com>
2025-08-31 12:05:24 +0530

    basic serpapi examlpe added with env loading

--- 09-langgraph/serapi_search_example.py
+++ 09-langgraph/serapi_search_example.py
@@ -1,6 +1,9 @@
 from langchain_community.utilities import SerpAPIWrapper
+from dotenv import load_dotenv
+load_dotenv()
 
-search = SerpAPIWrapper(serpapi_api_key="4f4e20ae4db0c405712f474d569913067eb217e2ed5b46653530b73dd05aa48a")
+search = SerpAPIWrapper()
 
-results = search.run("What is LangGraph?")
-print(results)
+result = search.run("What is today's news on Nvidia?")
+
+print(result)
