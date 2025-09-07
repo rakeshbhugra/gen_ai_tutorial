a46b652bf802e801cdddc891e963ba22b3a52510
RakeshBhugra-Vurbalize <rakesh@vurbalize.com>
2025-09-07 13:17:20 +0530

    fixed routing algorithm, there was issue with class name

--- capstone_project_email_classifier_live_coding/routing_algos/route_by_classification.py
+++ capstone_project_email_classifier_live_coding/routing_algos/route_by_classification.py
@@ -1,9 +1,9 @@
 from state import State
 
 def route_by_classification(state: State):
-    if state.classification == "sales_agent":
+    if state.classification == "sales":
         return "sales_agent"
-    elif state.classification == "customer_support_agent":
+    elif state.classification == "customer_support":
         return "customer_support_agent"
     else:
         return "human_in_the_loop"
