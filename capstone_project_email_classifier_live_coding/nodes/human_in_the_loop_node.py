d51a06c80d7d6d2c29875393f303cf71e0117782
RakeshBhugra-Vurbalize <rakesh@vurbalize.com>
2025-09-13 08:37:29 +0530

    updating state in human in the loop

--- capstone_project_email_classifier_live_coding/nodes/human_in_the_loop_node.py
+++ capstone_project_email_classifier_live_coding/nodes/human_in_the_loop_node.py
@@ -3,4 +3,7 @@ from state import State
 def human_in_the_loop_node(state: State):
     print("Routing to Human in the Loop...")
     # mock routing logic
+    state.email_to = "supportagent@example.com"
+    state.email_subject = "Response to your query"
+    state.email_body = "Please help the user with their query."
     return state
