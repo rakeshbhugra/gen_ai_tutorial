b437c40d4c7bd9177609127ba6c61141b5586002
RakeshBhugra-Vurbalize <rakesh@vurbalize.com>
2025-09-13 08:37:33 +0530

    removed comments

--- capstone_project_email_classifier_live_coding/vector_search_helper/query_chroma_db.py
+++ capstone_project_email_classifier_live_coding/vector_search_helper/query_chroma_db.py
@@ -41,7 +41,6 @@ def create_embeddings(text, model="models/text-embedding-004"):
         'model': model
     }
 
-
 def setup_chromadb():
     """Initialize ChromaDB client and collection."""
     client = chromadb.Client(Settings(
@@ -59,26 +58,6 @@ def setup_chromadb():
 
 # initialize ChromaDB client and collection
 client, collection = setup_chromadb()
-
-# query = "How many closed tickets do you review?"
-# # create embeddings for the query
-# query_embedding = create_embeddings(query)['embedding']
-
-# perform the query on the collection
-# results = collection.query(
-#     query_embeddings=[query_embedding],
-#     n_results=2,
-#     include=["documents", "metadatas", "distances"]
-# )
-
-# # print the results
-# print("Query Results:")
-# for i, doc in enumerate(results['documents'][0]):
-#     print(f"Result {i + 1}:")
-#     print(f"Document: {doc}")
-#     print(f"Metadata: {results['metadatas'][0][i]}")
-#     print(f"Distance: {results['distances'][0][i]}")
-#     print("-" * 40)  # Separator for readability
 
 def search_documents(query, num_results=3):
     """
