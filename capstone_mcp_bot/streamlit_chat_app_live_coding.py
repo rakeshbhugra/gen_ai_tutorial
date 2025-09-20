12e6ef4fb132dde6729f67c36824b0aaeda5e7ce
RakeshBhugra-Vurbalize <rakesh@vurbalize.com>
2025-09-20 12:45:51 +0530

    created basic echo bot

--- capstone_mcp_bot/streamlit_chat_app_live_coding.py
+++ capstone_mcp_bot/streamlit_chat_app_live_coding.py
@@ -11,7 +11,9 @@ def main():
     st.markdown("---")  # Horizontal line separator
 
     if "messages" not in st.session_state:
-        st.session_state.messages = []  # Empty list to store conversation history
+        st.session_state.messages = [
+            {"role": "assistant", "content": "Hello! How can I assist you today?"}
+        ]  # Empty list to store conversation history
 
     for message in st.session_state.messages:
         # Use Streamlit's chat_message component for proper styling
@@ -21,8 +23,19 @@ def main():
     prompt = st.chat_input("Ask me anything...")
     
     if prompt:
-        print("User prompt:", prompt)
-    
+        st.session_state.messages.append(
+            {"role": "user", "content": prompt}
+        )
+
+        with st.chat_message("user"):
+            st.markdown(prompt)
+
+        with st.chat_message("assistant"):
+            # echo the user response
+            response = f"echo: {prompt}"
+            st.markdown(response)
+
+        st.session_state.messages.append({"role": "assistant", "content": response})
 
 if __name__ == "__main__":
     main()
