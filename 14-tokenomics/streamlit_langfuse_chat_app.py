1dfb8bafa19b7b8caa61a23e918d5462c52fc657
RakeshBhugra-Vurbalize <rakesh@vurbalize.com>
2025-09-20 13:52:43 +0530

    added chatbot connected with langfuse

---
+++ 14-tokenomics/streamlit_langfuse_chat_app.py
@@ -0,0 +1,63 @@
+import streamlit as st
+import litellm
+from litellm import completion
+from dotenv import load_dotenv
+load_dotenv()
+
+litellm.success_callback = ["langfuse"]
+litellm.failure_callback = ["langfuse"] # logs errors to langfuse
+
+def get_response(messages: list[dict]) -> str:
+    model = 'openai/gpt-4.1-mini'
+    response = completion(
+        model=model,
+        messages=messages,
+        metadata = {
+            "trace_user_id": "rakesh_bhugra_001",
+            "session_id": "mcp_bot_session_002",
+            "tags":[
+                "agent2",
+            ]
+        }
+    )
+
+    return response.choices[0].message['content']
+
+def main():
+    st.set_page_config(
+        page_title="AI Chatbot",    # Browser tab title
+        page_icon="🤖",           # Browser tab icon
+        layout="centered"         # Page layout style
+    )
+
+    st.title("🤖 AI Chatbot")
+    st.markdown("---")  # Horizontal line separator
+
+    if "messages" not in st.session_state:
+        st.session_state.messages = [
+            {"role": "assistant", "content": "Hello! How can I assist you today?"}
+        ]  # Empty list to store conversation history
+
+    for message in st.session_state.messages:
+        # Use Streamlit's chat_message component for proper styling
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+    
+    prompt = st.chat_input("Ask me anything...")
+    
+    if prompt:
+        st.session_state.messages.append(
+            {"role": "user", "content": prompt}
+        )
+
+        with st.chat_message("user"):
+            st.markdown(prompt)
+
+        with st.chat_message("assistant"):
+            response = get_response(st.session_state.messages)
+            st.markdown(response)
+
+        st.session_state.messages.append({"role": "assistant", "content": response})
+
+if __name__ == "__main__":
+    main()
